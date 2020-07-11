eb2e660e7b580c66950d4b7737511cab10a5c609
Ibrahimsyah Z <ibra.himsyah10@gmail.com>
2020-07-11 11:25:51 +0700

    initial commit

---
+++ content.js
@@ -0,0 +1,84 @@
+module.exports = `
+  <h1>Heading 1</h1>
+  <h2>Heading 2</h2>
+  <h3>Heading 3</h3>
+  <h4>Heading 4</h4>
+  <h5>Heading 5</h5>
+  <h6>Heading 6</h6>
+  <p><br /></p>
+  <p>
+    <span style="color: rgb(0, 0, 0);"
+      >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.</span
+    >
+  </p>
+  <p><br /></p>
+  <p>
+    <span style="color: rgb(0, 0, 0);"
+      >Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+      accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+      illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+      explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+      odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+      voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+      quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
+      eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+      voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
+      corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+      Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
+      nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
+      nulla pariatur?</span
+    >
+  </p>
+  <ol>
+    <li><span style="color: rgb(0, 0, 0);">List1</span></li>
+    <li><span style="color: rgb(0, 0, 0);">List2</span></li>
+    <li><span style="color: rgb(0, 0, 0);">List 3</span></li>
+    <li><span style="color: rgb(0, 0, 0);">List 4</span></li>
+  </ol>
+  <p><br /></p>
+  <p>
+    <span style="color: rgb(0, 0, 0);"
+      >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.</span
+    >
+  </p>
+  <p><br /></p>
+  <p>
+    <span style="color: rgb(0, 0, 0);"
+      >Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+      accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+      illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+      explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+      odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+      voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+      quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
+      eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+      voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
+      corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+      Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
+      nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
+      nulla pariatur?</span
+    >
+  </p>
+  <ul>
+    <li><span style="color: rgb(0, 0, 0);">OL1</span></li>
+    <li><span style="color: rgb(0, 0, 0);">OL2</span></li>
+    <li><span style="color: rgb(0, 0, 0);">OL3</span></li>
+  </ul>
+  <p>
+    <img
+      src="https://dev-storage-monulis.mocogawe.com/monulis/images/7ledw7t-6a0109dd1e805f23072cd5474c3652460f5b724b294d28c4bc9ce4525181297d.jpg"
+    />
+  </p>
+`;
---
+++ index.js
@@ -0,0 +1,74 @@
+const puppeteer = require("puppeteer");
+const merge = require("easy-pdf-merge");
+const path = require("path");
+const fs = require("fs");
+
+const tempPath = "temp";
+
+const buildCover = async (browser) => {
+  const coverPath = `${tempPath}/cover.pdf`;
+  const pageBreak = '<p style="page-break-after: always;"> </p>';
+  const cover =
+    `<img class="cover-image" src="https://wallpapercave.com/wp/wp3611699.jpg"/>` +
+    pageBreak;
+  const page = await browser.newPage();
+  const options = {
+    pdf: "A4",
+    printBackground: true,
+    path: coverPath,
+  };
+  await page.setContent(cover);
+  await page.addStyleTag({
+    content: `
+  @page:{margin:0}
+  .cover-image {
+    top: 0;
+    left: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  `,
+  });
+  await page.pdf(options);
+  await page.close();
+  return coverPath;
+};
+
+const buildContent = async (browser) => {
+  const css = path.join(__dirname + "/layout.css");
+  const html = require("./content");
+  const contentPath = `${tempPath}/content.pdf`;
+  const options = {
+    pdf: "A4",
+    printBackground: true,
+    path: contentPath,
+    margin: { left: "2cm", top: "2cm", right: "1cm", bottom: "2.5cm" },
+  };
+  const page = await browser.newPage();
+  await page.setContent(html);
+  await page.addStyleTag({ path: css });
+  await page.pdf(options);
+  await page.close();
+  return contentPath;
+};
+
+const main = async () => {
+  const startTime = Date.now();
+  const resultFileName = "result.pdf";
+  !fs.existsSync(`./${tempPath}`) ? fs.mkdirSync(`./${tempPath}`) : "";
+  const browser = await puppeteer.launch();
+  Promise.all([buildContent(browser), buildCover(browser)])
+    .then((res) => {
+      merge([res[1], res[0]], resultFileName, (err) => {
+        const endTime = Date.now();
+        browser.close();
+        console.log("time: ", endTime - startTime);
+        console.log(err ? err : "done");
+      });
+    })
+    .catch((err) => console.error(err));
+};
+
+main();
